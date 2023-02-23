27d3a1fae9f5edb1f41ab8a1f818ddf6a53a6ec7
Erik Parso <parsoerik@gmail.com>
2023-02-23 20:36:50 +0100

    control panel

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,12 @@ type Car = {
 function App() {
 
 	const [cars, setCars] = useState<Car[]>([]);
+	const [name, setName] = useState('');
+	const [price, setPrice] = useState(0);
+	const [brand, setBrand] = useState(Brand.Skoda);
 
 	const handleAddButtonClick = () => {
-		const car: Car = { brand: Brand.Audi, name: 'A4', price: 20000 };
+		const car: Car = { brand: brand, name: name, price: price };
 		const newCars = cars.concat(car);
 		setCars(newCars);
 	};
@@ -22,7 +25,38 @@ function App() {
 	return (
 		<div>
 			<div>
-				<button onClick={handleAddButtonClick}>Add</button>
+				name: <input 
+					value={name} 
+					onChange={(event) => setName(event.target.value)}></input>
+
+				price: <input 
+					value={price} 
+					onChange={(event) => setPrice(+event.target.value)} 
+					type='number'></input>
+
+				Skoda: <input
+					checked={brand === Brand.Skoda} 
+					onClick={() => setBrand(Brand.Skoda)}
+					type='radio'></input>
+
+				Audi: <input 
+					checked={brand === Brand.Audi} 
+					onClick={() => setBrand(Brand.Audi)}
+					type='radio'></input>
+
+				BMW: <input 
+					checked={brand === Brand.BMW} 
+					onClick={() => setBrand(Brand.BMW)}
+					type='radio'></input>
+
+				Mercedes: <input 
+					checked={brand === Brand.Mercedes} 
+					onClick={() => setBrand(Brand.Mercedes)}
+					type='radio'></input>
+
+				<button 
+					disabled={price > 1000 || name.length === 0} 
+					onClick={handleAddButtonClick}>Add</button>
 			</div>
 
 			<div className="app">
